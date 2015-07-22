5adb21c234ac78b846f410f5c19d4911c23abf4e
jwz2kn <jwz2kn@virginia.edu>
2015-07-22 15:26:57 -0400

    Revert "ACS 2.0"
    This reverts commit a4be1084a6de40c9fe1f0f52c1b061aa8301fdad.

--- AlbemarleServicesAndroid/app/src/main/java/com/example/student/albemarleservicesandroid/MainActivity.java
+++ AlbemarleServicesAndroid/app/src/main/java/com/example/student/albemarleservicesandroid/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.student.albemarleservicesandroid;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 /*
 Converting c# version of the Albemarle Services app to java.
@@ -18,71 +14,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Get our buttons from the layout resource, the axml, in which we set their Android layout IDs
-        // Each button declaration followed by its own onclicklistener to tell it what to do
-        Button report = (Button) findViewById(R.id.report);
-        report.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action specified in ReportActivity
-                Intent i = new Intent(MainActivity.this, ReportActivity.class);
-                startActivity(i);
-            }
-        });
-
-        Button zoning = (Button) findViewById(R.id.zoning);
-        zoning.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-            }
-        });
-
-        Button plants = (Button) findViewById(R.id.plants);
-        plants.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-            }
-        });
-
-        Button certificate = (Button) findViewById(R.id.certificate);
-        certificate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click, take to website
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(
-                        "http://www.vdh.virginia.gov/Vital_Records/"));
-                startActivity(i);
-            }
-        });
-        Button contact = (Button) findViewById(R.id.contact);
-        contact.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action specified in ContactActivity
-                Intent i = new Intent(MainActivity.this, ContactActivity.class);
-                startActivity(i);
-            }
-        });
-
     }
 
-    // Should we have a settings menu that maybe takes you back to the main page?
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
